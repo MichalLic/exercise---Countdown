9594a58810e3afc34df5c68bc4fff67ccc35c618
Michal <m14lickiewicz@gmail.com>
2017-03-16 11:24:06 +0100

    Functions fixed, styling app.

--- src/js/timeout.js
+++ src/js/timeout.js
@@ -10,23 +10,27 @@ var Timeout = {
     },
 
     //function
-    getActuallyTime: function () {
+    setCurrentTime: function () {
         var myDate = new Date();
         var actualTime = myDate.toLocaleTimeString();
-        $('.time-block').html(actualTime);
+        $('.current-time').html(actualTime);
     },
 
     setInterval: function () {
         setInterval(function () {
-            Timeout.getActuallyTime()
+            Timeout.setCurrentTime()
         }, 1000);
     },
 
     onSend: function (btn) {
         $(btn).on('click', function () {
             Timeout.getFormValue($('.set-time'));
+            Timeout.addClassToShow();
             Timeout.durationEvent();
         });
+    },
+    addClassToShow: function (){
+        $('.difference-section').addClass('visible');
     },
 
     getFormValue: function (form) {
@@ -41,28 +45,22 @@ var Timeout = {
     durationEvent: function () {
         var dateValue = Timeout.getFormValue($('.set-time'));
         var dateStart = new Date();
-        var dateStop = new Date(dateValue.year, dateValue.month, dateValue.day, dateValue.hour, dateValue.minute);
+        //var dateStop = new Date(dateValue.year, dateValue.month, dateValue.day, dateValue.hour, dateValue.minute);
+        var dateStop = new Date(2017,2,16,11,55,0);
+
 
         var a = dateStart.toLocaleTimeString();
         var b = dateStop.toLocaleTimeString();
 
         var difference = Math.abs(dateStop.getTime() - dateStart.getTime());
-        //console.log(difference / 1000);
-        //var daysTo = Math.ceil(difference / (1000 * 60 * 60 * 24));
-        var secondsTo = Math.ceil(difference / 1000);
-        //console.log(daysTo);
+        var secDifference = difference / 1000;
 
-        //var dni = Math.round(difference / 1000 / 3600 / 24);
-        //console.log(dni);
-        //var godzin = Math.ceil(difference / 1000 / 3600);
-        //console.log(godzin);
-        //var minut = Math.ceil(difference / 1000 / 60);
-        //console.log(minut);
-        //var sekund = Math.ceil(difference / 1000);
-        //console.log(sekund)
-        //$('.difference').html(da);
+        var days = Math.floor(secDifference / 3600 / 24);
+        var hours = Math.floor(secDifference / 3600 % 24);
+        var minutes = Math.floor(secDifference / 60 % 60);
+        var seconds = Math.floor(secDifference % 60);
 
-        $('.difference').html(secondsTo);
+        $('.remained').html('Days: ' + days + ' Hours: ' + hours + ' Minutes: ' + minutes + ' Seconds: ' + seconds);
         setInterval(function(){Timeout.durationEvent()},1000);
     }
 
